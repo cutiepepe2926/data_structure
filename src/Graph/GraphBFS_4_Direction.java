047a6cfd8ec414c16797be531d3afc3862c87000
cutiepepe2926 <cutiepepe2926@gmail.com>
2026-01-23 17:09:55 +0900

    0123 - study graph bfs moving left,right,up,down

--- src/Graph/GraphBFS_4_Direction.java
+++ src/Graph/GraphBFS_4_Direction.java
@@ -1,10 +1,9 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.StringTokenizer;
+package Graph;
 
-public class Main {
+import java.io.*;
+import java.util.*;
+
+public class GraphBFS_4_Direction {
 
 
     static int n;
